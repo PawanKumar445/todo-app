ac8319aeacd724e5e62a806efcef40b074fada5d
Pawan kumar <kumarpawansahay445@gmail.com>
2024-09-24 17:21:42 +0530

    added delete functionality

--- src/todo/MainComponent.tsx
+++ src/todo/MainComponent.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
+import IconDelete from "../Icons/delete.tsx";
 
 export const MainComponent = () => {
-  
   const [todoList, setTodoList] = useState<Todo[]>([
     {
-      value: "Task1",
+      value: "eat healty food",
       checked: false,
       isCompleted: false,
     },
     {
-      value: "Task2",
+      value: "Daily got gym to buiild a fit body",
       checked: false,
       isCompleted: false,
     },
@@ -47,27 +47,41 @@ export const MainComponent = () => {
   const handleCheckBox = (clickedTodo: Todo) => {
     const updatedTotoList: any[] = todoList.map((t) => {
       if (t?.value === clickedTodo?.value) {
-        return { ...t, checked: !clickedTodo.checked, isCompleted: !clickedTodo.isCompleted };
+        return {
+          ...t,
+          checked: !clickedTodo.checked,
+          isCompleted: !clickedTodo.isCompleted,
+        };
       } else return t;
     });
     setTodoList(updatedTotoList);
   };
 
+  const handleDelete=(todoClicked: Todo )=>{
+    
+    
+    const updateTodoList= todoList.filter((t: Todo)=>
+    (todoClicked.value !== t.value)
+    );
+    console.log('Updated todo list is:', updateTodoList);
+    
+    setTodoList(updateTodoList);
+  }
 
   return (
     <div className="mt-10 ms-28 p-5">
-      <div className="w-1/5">
+      <div className="">
         <p className="text-4xl font-bold font-serif">TODO LIST</p>
         <form className="w-full p-2 ">
           <input
             type="text"
             value={todo?.value}
             onInput={handleTodoInput}
-            className=" w-8/12 outline-none border-b-2"
+            className=" outline-none border-b-2 "
             placeholder="Enter todo here..."
           />
           <input
-            className="ms-3 border border-cyan-500 px-2 rounded-lg cursor-pointer hover:bg-slate-400"
+            className="ms-3 border border-red-800 px-2 py-1 rounded-lg cursor-pointer hover:bg-red-500 hover:text-white text-sm font-semibold"
             type="submit"
             value="Add Todo"
             onClick={handleSubmit}
@@ -78,7 +92,7 @@ export const MainComponent = () => {
             {todoList.map((todo: Todo, index: number) => (
               <li
                 key={index}
-                className="p-2 text-lg font-semibold font-serif bg-slate-100 my-2 hover:bg-orange-100 rounded-md"
+                className="group p-2 text-lg font-semibold font-serif bg-slate-100 my-3 hover:bg-gray-300 rounded-md"
               >
                 <input
                   type="checkbox"
@@ -87,18 +101,29 @@ export const MainComponent = () => {
                   onChange={(e) => handleCheckBox(todo)}
                 />
                 <span className="me-3 ">{index + 1}.</span>
+                {/* <div className="inline-block text-wrap w-1/3"> */}
+                  <span
+                    contentEditable
+                    suppressContentEditableWarning
+                    className={`${
+                      todo?.checked ? "line-through" : ""
+                    } outline-none text-balance`}
+                  >
+                    {todo?.value}
+                  </span>
+                {/* </div> */}
                 <span
-                  contentEditable
-                  suppressContentEditableWarning
-                  className={`${
-                    todo?.checked ? "line-through" : ""
-                  } outline-none`}
+                  className={`text-sm font-normal mx-5 ${
+                    todo?.isCompleted ? "text-lime-600" : "text-red-600"
+                  } my-auto`}
                 >
-                  {todo?.value}
+                  {todo?.isCompleted ? "completed" : "pending"}
                 </span>
-                <span className={`text-sm font-normal mx-5 ${todo?.isCompleted? 'text-lime-600':'text-red-600'}`}>
-                  {todo?.isCompleted?'completed':'pending'}
-                </span>
+                <div className="w-7 inline-block  opacity-0 group-hover:opacity-100 cursor-pointer  ">
+                  <span onClick={() => handleDelete(todo)}>
+                    <IconDelete />
+                  </span>
+                </div>
               </li>
             ))}
           </ul>
